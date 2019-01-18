9075fc23413c6c47da0010b406e20ee726d9f839
RUNGNUENG <rungnueng19@hotmail.com>
2019-01-18 14:29:43 +0700

    create file

---
+++ app/src/main/java/com/mocom/com/mdancingproject/Activities/DashboardActivity.java
@@ -0,0 +1,15 @@
+package com.mocom.com.mdancingproject.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mocom.com.mdancingproject.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
--- app/src/main/java/com/mocom/com/mdancingproject/Activities/LoginActivity.java
+++ app/src/main/java/com/mocom/com/mdancingproject/Activities/LoginActivity.java
@@ -1,13 +1,13 @@
-package com.mocom.com.mdancingproject;
+package com.mocom.com.mdancingproject.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DashboardActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_login);
     }
 }
