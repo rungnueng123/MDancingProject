66a2af62c36dcb20a8330ca69bf1db308c61e88d
RUNGNUENG <rungnueng19@hotmail.com>
2019-01-24 17:11:47 +0700

    finish gen QRCode + scan for result

---
+++ app/src/main/java/com/mocom/com/mdancingproject/QRCode/AdminQRCodeScannedActivity.kt
@@ -0,0 +1,127 @@
+package com.mocom.com.mdancingproject.QRCode
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.TextView
+import android.widget.Toast
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.Result
+import com.mocom.com.mdancingproject.R
+import kotlinx.android.synthetic.main.activity_admin_qrcode_scanned.*
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+
+class AdminQRCodeScannedActivity : AppCompatActivity(), ZXingScannerView.ResultHandler, View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_admin_qrcode_scanned)
+
+        initInstance()
+    }
+
+    private fun initInstance() {
+        setScannerProperties()
+    }
+
+    override fun onClick(v: View?) {
+        if(v == barcodeBackImageView){
+            onBackPressed()
+        }
+        if(v == flashOnOffImageView){
+            if (qrCodeScanner.flash) {
+                qrCodeScanner.flash = false
+                flashOnOffImageView.background = ContextCompat.getDrawable(this, R.drawable.flash_off_vector_icon)
+            } else {
+                qrCodeScanner.flash = true
+                flashOnOffImageView.background = ContextCompat.getDrawable(this, R.drawable.flash_on_vector_icon)
+            }
+        }
+    }
+
+    private fun setScannerProperties() {
+        qrCodeScanner.setFormats(listOf(BarcodeFormat.QR_CODE))
+        qrCodeScanner.setAutoFocus(true)
+        qrCodeScanner.setLaserColor(R.color.colorAccent)
+        qrCodeScanner.setMaskColor(R.color.colorAccent)
+        if (Build.MANUFACTURER.equals(HUAWEI, ignoreCase = true))
+            qrCodeScanner.setAspectTolerance(0.5f)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),
+                        MY_CAMERA_REQUEST_CODE)
+                return
+            }
+        }
+        qrCodeScanner.startCamera()
+        qrCodeScanner.setResultHandler(this)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                openCamera()
+            else if (grantResults[0] == PackageManager.PERMISSION_DENIED)
+                showCameraSnackBar()
+        }
+    }
+
+    private fun showCameraSnackBar() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            val snackbar = Snackbar.make(scanQrCodeRootView, resources.getString(R.string.app_needs_your_camera_permission_in_order_to_scan_qr_code), Snackbar.LENGTH_LONG)
+            val view1 = snackbar.view
+            view1.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+            val textView = view1.findViewById<TextView>(android.support.design.R.id.snackbar_text)
+            textView.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary))
+            snackbar.show()
+        }
+    }
+
+    private fun openCamera() {
+        qrCodeScanner.startCamera()
+        qrCodeScanner.setResultHandler(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        qrCodeScanner.stopCamera()
+    }
+
+    override fun handleResult(result: Result?) {
+        if (result != null) {
+            startActivity(ScannedActivity.getScannedActivity(this, result.text))
+            resumeCamera()
+        }
+    }
+
+    private fun resumeCamera() {
+        Toast.LENGTH_LONG
+        val handler = Handler()
+        handler.postDelayed({ qrCodeScanner.resumeCameraPreview(this@AdminQRCodeScannedActivity) }, 2000)
+    }
+
+    companion object {
+        private const val HUAWEI = "huawei"
+        private const val MY_CAMERA_REQUEST_CODE = 6515
+        fun getScanQrCodeActivity(callingClassContext: Context) = Intent(callingClassContext, AdminQRCodeScannedActivity::class.java)
+    }
+}
