f12eab5a3cf4d1dc264a435f6d409290c574230f
RUNGNUENG <rungnueng19@hotmail.com>
2019-01-25 09:42:37 +0700

    config youtube api key

--- app/src/main/java/com/mocom/com/mdancingproject/config/config.java
+++ app/src/main/java/com/mocom/com/mdancingproject/config/config.java
@@ -1,6 +1,10 @@
 package com.mocom.com.mdancingproject.config;
 
 public class config {
+
+    public config(){
+
+    }
     //บ้านศรีพงษ์
 //    public static final String DATA_URL = "http://192.168.100.5/MDancingPHP/";
     //ที่ทำงาน
@@ -9,4 +13,11 @@ public class config {
 //    public static final String DATA_URL = "http://192.168.1.37/MDancingPHP/";
     //host
     public static final String DATA_URL = "https://danceschool.matchbox-station.com/MDancingPHP/";
+
+    //Youtube
+    private static final String YOUTUBE_KEY = "AIzaSyCxHiH9LBRrBpVtRREchulc_3e5hSdBdaA";
+
+    public static String getYoutubeKey() {
+        return YOUTUBE_KEY;
+    }
 }
