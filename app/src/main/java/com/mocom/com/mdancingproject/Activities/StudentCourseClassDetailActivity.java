0eb158e62c8df52d68a586ced0026cc5abef8f80
RUNGNUENG <rungnueng19@hotmail.com>
2019-02-05 09:31:16 +0700

    add up button and set title on toolbar

--- app/src/main/java/com/mocom/com/mdancingproject/Activities/StudentCourseClassDetailActivity.java
+++ app/src/main/java/com/mocom/com/mdancingproject/Activities/StudentCourseClassDetailActivity.java
@@ -3,12 +3,14 @@ package com.mocom.com.mdancingproject.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.mocom.com.mdancingproject.R;
 
 public class StudentCourseClassDetailActivity extends AppCompatActivity {
 
     String eventID;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +20,24 @@ public class StudentCourseClassDetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         eventID = intent.getStringExtra("eventID");
 //        Toast.makeText(this,eventID,Toast.LENGTH_LONG).show();
+
+        initInstance();
+    }
+
+    private void initInstance() {
+        initFindViewByID();
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        toolbar.setNavigationOnClickListener(v -> finish());
+    }
+
+    private void initFindViewByID() {
+        toolbar = findViewById(R.id.toolbar_course_class_detail_single);
     }
 }
