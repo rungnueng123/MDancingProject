ea69adf3812617f9a8639b988a882065029c8bc5
RUNGNUENG <rungnueng19@hotmail.com>
2019-02-25 15:53:09 +0700

    buy class by coin if coin is enough

--- app/src/main/java/com/mocom/com/mdancingproject/Activities/ClassDetailActivity.java
+++ app/src/main/java/com/mocom/com/mdancingproject/Activities/ClassDetailActivity.java
@@ -2,7 +2,10 @@ package com.mocom.com.mdancingproject.Activities;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -19,6 +22,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.mocom.com.mdancingproject.DialogFragment.PaymentClassDialog;
+import com.mocom.com.mdancingproject.DialogFragment.TypePaymentClassDialog;
 import com.mocom.com.mdancingproject.R;
 
 import org.json.JSONArray;
@@ -31,15 +35,45 @@ import java.util.Map;
 import static com.mocom.com.mdancingproject.config.config.DATA_URL;
 import static com.mocom.com.mdancingproject.config.config.HOST_URL;
 
-public class ClassDetailActivity extends AppCompatActivity implements View.OnClickListener {
+public class ClassDetailActivity extends AppCompatActivity implements View.OnClickListener, TypePaymentClassDialog.OnInputListener {
+
+    public static final int COIN_CAN_PAY_CODE = 1;
 
     String getClassUrl = DATA_URL + "get_single_class.php";
-    String eventID, coinAmt, eventName, canBuy;
+    String canBuyClassByCoinUrl = DATA_URL + "buy_class_by_coin.php";
+    String eventID, coinAmt, eventName, userID, canBuy;
     Toolbar toolbar;
 
     ImageView imgClass, imgArrow;
     TextView txtClassName, txtPlaylist, txtStyle, txtTeacher, txtDate, txtTime, txtEmpty, txtBranch, txtCoin, txtDesc;
+    public TextView txtTypePay;
     Button btnPayment;
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    public void sendInput(String input) {
+        txtTypePay.setText(input);
+        if (txtTypePay.getText().toString().equals(getResources().getString(R.string.txt_type_payment_coin))) {
+            Intent intent = new Intent(getApplicationContext(), CoinTypePaymentClassActivity.class);
+            intent.putExtra("coin", coinAmt);
+            intent.putExtra("eventID", eventID);
+            intent.putExtra("eventName", eventName);
+            startActivityForResult(intent, COIN_CAN_PAY_CODE);
+        } else if (txtTypePay.getText().toString().equals(getResources().getString(R.string.txt_type_payment_style_pack))) {
+
+        }
+    }
+
+//    @Override
+//    public void sendCanPayByCoin(String input) {
+//        if (input.equals("Your coin don't enough! Please go to shop!")) {
+//
+//        } else if (input.equals("enough")) {
+//            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+//            userID = sharedPreferences.getString(getString(R.string.UserID), "");
+//            goBuyClass(eventID, userID);
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +82,8 @@ public class ClassDetailActivity extends AppCompatActivity implements View.OnCli
 
         Intent intent = getIntent();
         eventID = intent.getStringExtra("eventID");
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        userID = sharedPreferences.getString(getString(R.string.UserID), "");
 //        Toast.makeText(this, eventID, Toast.LENGTH_SHORT).show();
 
         initFindViewByID();
@@ -136,6 +172,7 @@ public class ClassDetailActivity extends AppCompatActivity implements View.OnCli
         txtDesc = findViewById(R.id.txt_show_desc);
         btnPayment = findViewById(R.id.btn_payment);
         btnPayment.setOnClickListener(this);
+        txtTypePay = findViewById(R.id.txt_type_pay);
 
     }
 
@@ -151,9 +188,10 @@ public class ClassDetailActivity extends AppCompatActivity implements View.OnCli
             }
         }
         if (v == btnPayment) {
-            if(canBuy.equals("can")) {
-                openDialogFragment(coinAmt, eventID, eventName);
-            }else{
+            if (canBuy.equals("can")) {
+                openDialogTypePay(coinAmt, eventID, eventName);
+//                openDialogFragment(coinAmt, eventID, eventName);
+            } else {
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setMessage("คลาสนี้เลยเวลาการสอนแล้ว ไม่สามารถซื้อได้")
                         .setNegativeButton("ok", null);
@@ -161,6 +199,16 @@ public class ClassDetailActivity extends AppCompatActivity implements View.OnCli
                 alert.show();
             }
         }
+    }
+
+    private void openDialogTypePay(String coinAmt, String eventID, String eventName) {
+        Bundle bundle = new Bundle();
+        bundle.putString("coin", coinAmt);
+        bundle.putString("eventID", eventID);
+        bundle.putString("eventName", eventName);
+        TypePaymentClassDialog dialog = new TypePaymentClassDialog();
+        dialog.setArguments(bundle);
+        dialog.show(getSupportFragmentManager(), "TypePaymentClassDialog");
     }
 
     private void openDialogFragment(String coinAmt, String eventID, String eventName) {
@@ -171,5 +219,58 @@ public class ClassDetailActivity extends AppCompatActivity implements View.OnCli
         PaymentClassDialog dialog = new PaymentClassDialog();
         dialog.setArguments(bundle);
         dialog.show(getSupportFragmentManager(), "PaymentClassDialog");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == COIN_CAN_PAY_CODE) {
+//            Toast.makeText(getApplicationContext(), data.getStringExtra("message"), Toast.LENGTH_SHORT).show();
+            if (data.getStringExtra("message").equals("Cancel")) {
+                Toast.makeText(getApplicationContext(), data.getStringExtra("message"), Toast.LENGTH_SHORT).show();
+            }
+            if (data.getStringExtra("message").equals("enough")) {
+                goBuyClass(userID, eventID);
+            }
+            if (data.getStringExtra("message").equals("Your coin don't enough! Please go to shop!")) {
+                Toast.makeText(getApplicationContext(), data.getStringExtra("message"), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void goBuyClass(String userID, String eventID) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, canBuyClassByCoinUrl, response -> {
+            Log.d("response", response);
+            try {
+                JSONObject obj = new JSONObject(response);
+                if (obj.getString("message").equals("Payment Success")) {
+                    Intent intent = new Intent(getApplicationContext(), StudentDashboardActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.putExtra("goProfile", "goProfile");
+                    startActivity(intent);
+                } else {
+                    goBuyClass(userID, eventID);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("eventID", eventID);
+                params.put("userID", userID);
+                params.put("coin", coinAmt);
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+//        Intent intent = new Intent(getActivity(), StudentDashboardActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK  |  Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        startActivity(intent);
     }
 }
